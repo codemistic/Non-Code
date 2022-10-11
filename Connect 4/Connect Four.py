a25ecf52fcf2858baa5ca8ade2cccfc5cbe2fa4f
Krishna Rajkumar Dembla <krishnadembla2001@gmail.com>
2022-10-11 22:38:38 +0530

    by KryTax

---
+++ Connect 4/Connect Four.py
@@ -0,0 +1,50 @@
+from turtle import *
+
+from freegames import line
+
+turns = {'red': 'yellow', 'yellow': 'red'}
+state = {'player': 'yellow', 'rows': [0] * 8}
+
+
+def grid():
+    """Draw Connect Four grid."""
+    bgcolor('light blue')
+
+    for x in range(-150, 200, 50):
+        line(x, -200, x, 200)
+
+    for x in range(-175, 200, 50):
+        for y in range(-175, 200, 50):
+            up()
+            goto(x, y)
+            dot(40, 'white')
+
+    update()
+
+
+def tap(x, y):
+    """Draw red or yellow circle in tapped row."""
+    player = state['player']
+    rows = state['rows']
+
+    row = int((x + 200) // 50)
+    count = rows[row]
+
+    x = ((x + 200) // 50) * 50 - 200 + 25
+    y = count * 50 - 200 + 25
+
+    up()
+    goto(x, y)
+    dot(40, player)
+    update()
+
+    rows[row] = count + 1
+    state['player'] = turns[player]
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+grid()
+onscreenclick(tap)
+done()
